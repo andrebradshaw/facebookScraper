e36abae3cc70e354516685d0a752c32292b4b86f
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-10-18 22:15:57 -0400

    Update memberRequestJSONP.js

--- memberRequestJSONP.js
+++ memberRequestJSONP.js
@@ -1,16 +1,14 @@
 
 function elmConditionMatch(elmArr, str, cond){var x = new RegExp(str, "i");	if(cond == "t"){		for(i=0; i<elmArr.length; i++){			if(x.test(elmArr[i].innerText) === true){				return elmArr[i];			}		}	}	if(cond == "f"){		for(i=0; i<elmArr.length; i++){			if(x.test(elmArr[i].innerText) === false){				return elmArr[i];			}		}    }}
 function grp(elm,n){if(elm != null){return elm[n];}else{return "";}}
-
-
+function cln(str){return str.replace(/\?/, "_QST_").replace(/\#/, "_HSH_").replace(/\&/, "_AMP_");}
+var webAppUrl = "";
 var buttons = document.getElementById("member_requests_pagelet").getElementsByTagName("button");
 for(b=0; b<buttons.length; b++){
 	var itext = buttons[b].innerText;
 	if(itext == "Approve"){
 		var info = buttons[b].parentElement.parentElement.getElementsByTagName("ul")[0];
 		var atags = elmConditionMatch(buttons[b].parentElement.parentElement.getElementsByTagName("a"),"More Actions","f");
-
-
 		var joinQ = buttons[b].parentElement.parentElement.getElementsByTagName("ul")[3].getElementsByTagName("text")[0].innerText;	
 		var fullname = atags.innerText;
 		var linky = atags.href;
@@ -21,8 +19,8 @@ for(b=0; b<buttons.length; b++){
 		var livesIn = grp(/(?<=Lives in ).+/.exec(elmConditionMatch(info.getElementsByTagName("li"),"Lives in","t").innerText),0);
 		var geoLink = grp(/\d+/.exec(elmConditionMatch(info.getElementsByTagName("li"),"Lives in","t").getElementsByTagName("a")[0].href),0);//https://www.facebook.com/pages/
 
-var output = "?fn="+fullname+"&url="+linky+"frn="+friends+"&gc="+groupConn+"&og="+otherGroup+"&liv="+livesIn+"&geo="+geoLink+"&jq="+joinQ;
-		console.log(output);
+		var output = "?fn="+cln(fullname)+"&url="+cln(linky)+"frn="+friends+"&gc="+groupConn+"&og="+otherGroup+"&liv="+cln(livesIn)+"&geo="+cln(geoLink)+"&jq="+cln(joinQ);
+		window.open(webAppUrl+output);
 	}
 }
 
